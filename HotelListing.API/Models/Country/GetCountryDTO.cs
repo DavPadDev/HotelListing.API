5519398c48a6f398f10e7604c167d43d44c6adb8
dav.pad <dav.pad@davpaddev.com>
2023-08-08 12:40:16 -0500

    REST API Development Best Practices, Automapper and Repositories

---
+++ HotelListing.API/Models/Country/GetCountryDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Models.Country
+{
+    public class GetCountryDTO : BaseCountryDTO
+    {
+        public int Id { get; set; }
+    }
+}
